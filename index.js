00e634b062d8ea9541b484b982240fc321a5e0df
Paolo Insogna <paolo@cowtech.it>
2021-01-04 13:13:15 +0100

    feat: Replaced wrong file.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-module.exports = {
-  extends: ['standard'],
-  rules: {
-    curly: [2, 'all'],
-    /*
-      This is inserted to make this compatible with prettier.
-      Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
-    */
-    'space-before-function-paren': 0
+const { existsSync, lstatSync } = require('fs')
+const { join, dirname } = require('path')
+
+module.exports = function transform(file, api) {
+  const root = api.jscodeshift(file.source)
+  let changed = false
+
+  for (const type of [
+    api.jscodeshift.ImportDeclaration,
+    api.jscodeshift.ExportAllDeclaration,
+    api.jscodeshift.ExportNamedDeclaration
+  ]) {
+    root
+      .find(type)
+      .filter(declaration => declaration.value.source && declaration.value.source.type === 'Literal')
+      .forEach(declaration => {
+        let path = declaration.value.source.value
+
+        if (!path.startsWith('.')) {
+          // Not a bare import, skip it
+          return
+        }
+
+        // If it is a directory import, append index.js
+        const absolutePath = join(dirname(file.path), path)
+
+        if (existsSync(absolutePath) && lstatSync(absolutePath).isDirectory()) {
+          path = declaration.value.source.value += '/index'
+        }
+
+        if (!declaration.value.source.value.endsWith('.mjs')) {
+          declaration.value.source.value += '.mjs'
+          changed = true
+        }
+      })
   }
+
+  return changed ? root.toSource() : null
 }
